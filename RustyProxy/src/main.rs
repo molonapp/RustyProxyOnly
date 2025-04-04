b83775115a6983a2e6ba217e16bc069dda983d48
molonapp <168112275+molonapp@users.noreply.github.com>
2025-04-04 13:12:57 -0300

    Update main.rs

--- RustyProxy/src/main.rs
+++ RustyProxy/src/main.rs
@@ -41,13 +41,13 @@ async fn handle_client(mut client_stream: TcpStream) -> Result<(), Error> {
         .await?;
 
     let mut buffer = vec![0; 1024];
-    client_stream.read(&mut buffer).await?;
+    //client_stream.read(&mut buffer).await?;
     client_stream
         .write_all(format!("HTTP/1.1 200 {}\r\n\r\n", status).as_bytes())
         .await?;
 
     let mut addr_proxy = "0.0.0.0:22";
-    let result = timeout(Duration::from_secs(3), peek_stream(&mut client_stream)).await
+    let result = timeout(Duration::from_secs(1), peek_stream(&mut client_stream)).await
         .unwrap_or_else(|_| Ok(String::new()));
 
     if let Ok(data) = result {
